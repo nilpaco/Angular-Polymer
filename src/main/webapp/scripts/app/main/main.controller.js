ceb61db739bbd1e6e6e68b9d76539e03d5fa1a91
Nil Panés <nil.panes@gmail.com>
2016-01-17 23:27:38 +0100

    Added space search on the toolbar.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('phipsterApp')
-    .controller('MainController', function ($scope, Principal, Space) {
+    .controller('MainController', function ($scope, Principal, Space, ParseLinks) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
@@ -16,5 +16,22 @@ angular.module('phipsterApp')
                 }
             });
         };
+        $scope.spaces = [];
+        $scope.predicate = 'id';
+        $scope.reverse = true;
+        $scope.page = 1;
+        $scope.loadAll = function() {
+            Space.query({page: $scope.page - 1, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                $scope.totalItems = headers('X-Total-Count');
+                $scope.spaces = result;
+            });
+        };
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        $scope.loadAll();
+
 
     });
