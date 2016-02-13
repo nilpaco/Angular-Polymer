aa79ea1e9496e89d7c1e0f368afc5786ad12a6f3
Nil Panés <nil.panes@gmail.com>
2016-02-13 20:33:49 +0100

    CSS and filter on main.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,21 +1,12 @@
 'use strict';
 
 angular.module('phipsterApp')
-    .controller('MainController', function ($scope, Principal, Space, ParseLinks) {
+    .controller('MainController', function ($scope, DataUtils, $state, SpaceSearch, Principal, Space, ParseLinks) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
 
         });
-        $scope.search = function () {
-            SpaceSearch.query({query: $scope.searchQuery}, function(result) {
-                $scope.spaces = result;
-            }, function(response) {
-                if(response.status === 404) {
-                    $scope.loadAll();
-                }
-            });
-        };
         $scope.spaces = [];
         $scope.predicate = 'id';
         $scope.reverse = true;
@@ -32,6 +23,38 @@ angular.module('phipsterApp')
             $scope.loadAll();
         };
         $scope.loadAll();
+
+
+        $scope.search = function () {
+            SpaceSearch.query({query: $scope.searchQuery}, function(result) {
+                $scope.spaces = result;
+            }, function(response) {
+                if(response.status === 404) {
+                    $scope.loadAll();
+                }
+            });
+        };
+
+        $scope.refresh = function () {
+            $scope.loadAll();
+            $scope.clear();
+        };
+
+        $scope.clear = function () {
+            $scope.space = {
+                name: null,
+                img: null,
+                imgContentType: null,
+                description: null,
+                price: null,
+                id: null
+            };
+        };
+
+        $scope.abbreviate = DataUtils.abbreviate;
+
+        $scope.byteSize = DataUtils.byteSize;
+
 
 
     });
